161a555937d755f8b50667e12dd0c82178b49793
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-02-05 22:36:43 +0530

    aded hero content left details

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+
+import { discount, robot } from "../assets";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section>
+      <div>
+        <div>
+          <img src={discount} alt="discount" />
+          <p>
+            <span className="text-white">20%</span> Discount For <span className="text-white">1 Month</span> Account
+          </p>
+        </div> 
 
-export default Hero
+        <div>
+          <h1 className="text-white">
+            The Next <br /> <span className="text-gradient">Generation</span>{" "}
+          </h1>
+          <div></div>
+        </div>
+
+        <h1 className="text-white">Payment Method</h1>
+
+        <p className="text-white">
+          Our team of experts uses a methodology to identify the credit cards
+          most likely to fit your needs. We examine annual percentage rates,
+          annual fees.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
