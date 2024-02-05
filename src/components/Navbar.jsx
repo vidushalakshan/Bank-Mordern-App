ed6e694947e79e62885f52fa505ec2a7f13197e9
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-02-05 20:21:28 +0530

    aded style nav text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import {close, logo, menu} from "../assets";
+import { navLinks } from '../constants';
+import style from '../style';
 
 const Navbar = () => {
   return (
     <nav>
         <img src={logo} alt="navLogo" />
         <ul>
-            <li>
-                
-            </li>
+            {navLinks.map((nav, index) => (
+                <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length -1 ? "mr-0" : "mr-10"}`}
+                >
+                    <a href={`${nav.id}`}>{nav.title}</a>
+                </li>
+            ))}
         </ul>
     </nav>
   )
