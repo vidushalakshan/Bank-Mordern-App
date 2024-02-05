25b1f43dc67bdf0237c5c7c30f6cb2da080e9056
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-02-05 18:43:35 +0530

    aded navlinks constants

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from "react"
+import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
     <div>
-     <h1 className="text-red-500">Hello</h1>
+      <Navbar />
     </div>
   )
 }
