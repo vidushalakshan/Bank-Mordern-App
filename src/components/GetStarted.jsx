d71ea3aed1494b5a70c97d2ef203997ddae93b08
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-02-07 16:50:28 +0530

    create round button

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { arrowUp } from "../assets";
+import style from "../style";
 
 const GetStarted = () => {
   return (
-    <div>
-        <div>
-            <div>
+    <div className={`${style.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+        <div className={`${style.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+            <div className={`${style.flexStart} flex-row`}>
                 <p>
                     <span>
                         Get
