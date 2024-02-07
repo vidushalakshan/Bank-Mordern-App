05426db7cd7981b8c28a4bb41e0cdd1e5dc1545c
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-02-07 18:34:47 +0530

    aded robot image in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,30 +9,48 @@ const Hero = () => {
     <section className={`flex md:flex-row flex-col ${style.paddingY}`}>
       <div className={`flex ${style.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounden-[10px] mb-2">
-          <img src={discount} alt="discount" 
-          className="w-[32px] h-[32px]"
-          />
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${style.paragraph} ml-2`}>
-            <span className="text-white">20%</span> Discount For <span className="text-white">1 Month</span> Account
+            <span className="text-white">20%</span> Discount For{" "}
+            <span className="text-white">1 Month</span> Account
           </p>
-        </div> 
+        </div>
 
-        <div className="flex flex-row justify-between items-center w-full">
+        <div className="flex flex-row items-center justify-between w-full">
           <h1 className="text-white font-poppins font-semibold ss:text-[72px] text-[52px] ss:leading[100.8px]">
-            The Next <br className="sm:block hidden" /> <span className="text-gradient">Generation</span>{" "}
+            The Next <br className="hidden sm:block" />{" "}
+            <span className="text-gradient">Generation</span>{" "}
           </h1>
           <div>
             <GetStarted />
           </div>
         </div>
 
-        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full ">Payment Method</h1>
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full ">
+          Payment Method
+        </h1>
 
         <p className={`${style.paragraph} max-w-[470px] mt-5`}>
           Our team of experts uses a methodology to identify the credit cards
           most likely to fit your needs. We examine annual percentage rates,
           annual fees.
         </p>
+      </div>
+
+      <div className={`flex flex-1 ${style.flexCenter} md:my-0 my-10 relative`}>
+        <img
+          src={robot}
+          alt="robot"
+          className="w-[100%] h-[100%] relative z-[5]"
+        />
+
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+      </div>
+
+      <div className={`${style.flexCenter} ss:hidden`}>
+        <GetStarted />
       </div>
     </section>
   );
