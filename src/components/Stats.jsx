353101ab0be7397b89432f97e8fe52417675f1fd
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-02-08 12:54:53 +0530

    aded stats section text style

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -6,10 +6,10 @@ const Stats = () => {
     <div>
         {stats.map((stat) => (
             <div className='text-white'>
-                <h4>
+                <h4 className='font-semibold font-poppins xs:text-[40.89px] text-[30.86px] xs:leading-[53.16px] leading-[43.16px] text-white'>
                     {stat.value}
                 </h4>
-                <p>
+                <p className='font-normal font-poppins xs:text-[20.45px] text-[15.45px] xs:leading-[26.58px] leading-[21.58px] text-gradient uppercase ml-3'>
                     {stat.title}
                 </p>
             </div>
