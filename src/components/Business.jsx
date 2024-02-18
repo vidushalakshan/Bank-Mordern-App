e3be40f2fb4312648781bdc279594d10aa272c37
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-02-18 21:29:52 +0530

    updated featurecard title

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -34,8 +34,8 @@ const FeatureCard = ({icon, title, content, index}) => (
     <div className={`w-[64px] h-[64px] rounded-full ${style.flexCenter} bg-dimBlue`}>
       <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain"/>
     </div>
-    <div>
-      <h4>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
         {title}
       </h4>
       <p>
