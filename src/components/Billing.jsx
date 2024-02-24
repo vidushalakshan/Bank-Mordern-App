c895eb2053052637f5622293322aa9a88d890741
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-02-24 21:22:24 +0530

    aded billing section gradient line

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -11,6 +11,14 @@ const Billing = () => {
           alt="billing"
           className="w-[100%] h-[100%] relative z-[5]"
         />
+
+       
+   
+      {/* gradient start */}
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+      {/* gradient end */}
+
       </div>
 
       <div className={layout.sectionInfo}>
