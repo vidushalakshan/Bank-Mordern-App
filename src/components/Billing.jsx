4d93125ba3201b94fdbf1a7bd310bf5dd01fb268
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-02-24 21:13:09 +0530

    aded billing details and app store ,google play item

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import { apple, bill, google } from '../assets';
-import style,{ layout } from '../style';
+import React from "react";
+import { apple, bill, google } from "../assets";
+import style, { layout } from "../style";
 
 const Billing = () => {
   return (
     <section>
-        <div>
-            <img src={bill} alt="billing" className='w-[100%] h-[100%] relative z-[5]'/>
-        </div>
-    </section>
-  )
-}
+      <div>
+        <img
+          src={bill}
+          alt="billing"
+          className="w-[100%] h-[100%] relative z-[5]"
+        />
+      </div>
 
-export default Billing
+      <div>
+        <h2>
+          Easily control your <br /> billing & invoicing
+        </h2>
+
+        <p>
+          Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+          aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+          placerat.
+        </p>
+
+        <div>
+            <img src={apple} alt="apple_store" />
+            <img src={google} alt="google_play" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Billing;
