493502d865864005c2bcda9c8708962b1230bb71
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-02-26 15:06:12 +0530

    complete Testimonials section header

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from "react";
+import style from "../style";
 
 const Testimonials = () => {
   return (
-    <section>
-        <div>
-            <h2>
-                What People are <br /> Saying about us
-            </h2>
-            <div>
-                <p>
-                    Everything you need to accept card payment and grow your business anywhare on the planet.
-                </p>
-            </div>
+    <section
+      id="clients"
+      className={`${style.paddingY} ${style.flexCenter} flex-col relative`}
+    >
+      <div className="relative flex flex-col items-center justify-between w-full mb-6 md:flex-row sm:mb-16 z-[1]">
+        <h2 className={style.heading2}>
+            What People are <br /> Saying about us
+        </h2>
+        <div className='w-full mt-6 md:mt-0'>
+            <p className={`${style.paragraph} text-left max-w-[450px]`}>
+            Everything you need to accept card payment and grow your business
+            anywhare on the planet.
+            </p>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
