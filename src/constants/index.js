c01c182643e5dd540d4e1677ee967db24b2df49e
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-02-26 15:07:37 +0530

    aded feed back details

--- src/constants/index.js
+++ src/constants/index.js
@@ -59,3 +59,30 @@ export const features = [
         content: 'A balance transfer credit card can save you a lot of money in interest charges.',
     },
 ];
+
+export const feedback = [
+    {
+      id: "feedback-1",
+      content:
+        "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+      name: "Herman Jensen",
+      title: "Founder & Leader",
+      img: people01,
+    },
+    {
+      id: "feedback-2",
+      content:
+        "Money makes your life easier. If you're lucky to have it, you're lucky.",
+      name: "Steve Mark",
+      title: "Founder & Leader",
+      img: people02,
+    },
+    {
+      id: "feedback-3",
+      content:
+        "It is usually people in the money business, finance, and international trade that are really rich.",
+      name: "Kenn Gallagher",
+      title: "Founder & Leader",
+      img: people03,
+    },
+  ];
