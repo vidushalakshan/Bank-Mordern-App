bb3f18c4b50e6bb150b0a3796598160aa102c796
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-02-26 17:39:27 +0530

    create background CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Button from './Button';
+import style from '../style';
 
 
 const CTA = () => {
   return (
-    <section>
+    <section className={`${style.flexCenter} ${style.marginY} ${style.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
         <div>
             <h2>
                 Let's try our service now!
