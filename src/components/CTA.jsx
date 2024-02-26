c1b1cdda0d65361f08125104aa9ecb55c4c9b1ee
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-02-26 17:35:49 +0530

    aded details CTA section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import Button from './Button';
 
 
 const CTA = () => {
   return (
-    <div>
-        CTA
-    </div>
+    <section>
+        <div>
+            <h2>
+                Let's try our service now!
+            </h2>
+            <p>
+                Everything you need to accept card payments and grow you business anywhare on the planet.
+            </p>
+        </div>
+
+        <div>
+            <Button />
+        </div>
+    </section>
   )
 }
 
