e531429bb409206a53357b112197331fd99afb03
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-02-26 17:42:37 +0530

    completed CTA section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,16 +6,16 @@ import style from '../style';
 const CTA = () => {
   return (
     <section className={`${style.flexCenter} ${style.marginY} ${style.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
-        <div>
-            <h2>
+        <div className='flex flex-col flex-1'>
+            <h2 className={style.heading2}>
                 Let's try our service now!
             </h2>
-            <p>
+            <p className={`${style.paragraph} max-w-[470px] mt-5`}>
                 Everything you need to accept card payments and grow you business anywhare on the planet.
             </p>
         </div>
 
-        <div>
+        <div className={`${style.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
             <Button />
         </div>
     </section>
