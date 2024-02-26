a64fd02cb2eec14a28efe9019a76f1659aae3b76
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2024-02-26 18:15:44 +0530

    map footer links in footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { logo } from '../assets';
+import { footerLinks, socialMedia } from '../constants';
 
 const Footer = () => {
   return (
@@ -8,6 +9,27 @@ const Footer = () => {
             <div>
                 <img src={logo} alt="hoobank" />
             </div>
+            <p>
+                A new way to make the payments easy, reliable and secure.
+            </p>
+        </div>
+
+        <div>
+            {footerLinks.map((footerlink) => (
+                <div>
+                    <h4>
+                        {footerlink.title}
+                    </h4>
+
+                    <ul>
+                        {footerlink.links.map((link,index) => (
+                            <li>
+                                {link.name}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
         </div>
     </section>
   )
